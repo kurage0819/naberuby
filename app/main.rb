8c72a9f1c756a25d8d86d5d30619e758a4c2dc39
kurage0819 <whileinclude1@yahoo.co.jp>
2016-11-17 16:34:53 +0900

    first

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,21 @@
 def main(argv)
   argv.each do |v|
-    puts v
+  	vc=v.to_s
+  	if vc =~ /[^ -~｡-ﾟ]/
+  		printf("error\n")
+  		exit
+  	end
+
+  	if vc =~ /[^0-9]/
+  		puts "invaild"
+  	elsif v%3 == 0 && vc.index("3") != nil
+  		puts "dumb"
+  	elsif v%3 == 0
+  		puts "idiot"
+  	elsif vc.index("3") != nil
+  		puts "stupid"
+  	else
+  		puts "smart"
+  	end
   end
 end
